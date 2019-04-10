e16a0d95eb374251590344632bbb6280f2ace825
Giancarlo Palavicini <kasthor@gmail.com>
2019-04-10 10:50:55 -0600

    chore: rename main file

---
+++ lodash.deep_ops.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+
+module.exports = {
+  forInDeep(object, path, iteratee) {
+    if (_.isUndefined(iteratee)) {
+      iteratee = path;
+      path = '';
+    }
+
+    _.forIn(object, (value, key) => {
+      let childPath = _.without([path, key], '');
+
+      if (_.isArray(object)) {
+        const idx = childPath.pop();
+        childPath = `${childPath.join('.')}[${idx}]`;
+      } else {
+        childPath = childPath.join('.');
+      }
+
+      iteratee(value, key, childPath, object);
+
+      if (_.isObject(value)) {
+        _.forInDeep(value, childPath, iteratee);
+      }
+    });
+  },
+  filterKeyDeep(object, key, iteratee) {
+    _.forInDeep(object, (v, k, p, o) => {
+      if (key === k) iteratee(v, k, p, o);
+    });
+  },
+  mapValuesDeep(object, iteratee) {
+    return _.fromPairs(
+      _.map(object,
+        (v, k) => ((_.isObject(v))
+          ? [k, _.mapValuesDeep(v, iteratee)] : [k, _.iteratee(iteratee)(v)])),
+    );
+  },
+};
